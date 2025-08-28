9ac679739cbf1c8742c8d9a562ddfe2769f5cfc2
Mathias <mathias_sn@hotmail.com>
2025-08-28 22:21:45 +0200

    clean up

--- src/main/java/com/github/ars_affinity/effect/SkyflowEffect.java
+++ src/main/java/com/github/ars_affinity/effect/SkyflowEffect.java
@@ -7,20 +7,16 @@ import net.minecraft.world.entity.LivingEntity;
 public class SkyflowEffect extends MobEffect {
     
     public SkyflowEffect() {
-        super(MobEffectCategory.BENEFICIAL, 0x00FFFF); // Cyan color for water theme
-        // This effect is used as a marker for mana regeneration boost when wet
+        super(MobEffectCategory.BENEFICIAL, 0x00FFFF);
     }
     
     @Override
     public boolean applyEffectTick(LivingEntity entity, int amplifier) {
-        // This effect doesn't need to tick every frame
-        // The movement speed modifier is handled automatically by the effect system
         return true;
     }
     
     @Override
     public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
-        // This effect doesn't need to tick every frame
         return false;
     }
 }
